acad1565e91dbe2e72ec761ad1458c20b3e1c346
AlexanderA R1 <alexander.thequeen@gmail.com>
2020-06-29 23:01:41 +0300

    Initial Commit

---
+++ antOnTheCenter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  antOnTheCenter
+//
+//  Created by the Dragon on 29.06.2020.
+//  Copyright © 2020 theRAA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
